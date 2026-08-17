e966686753d508bb4d9f7dea90002df2e06a5e23
Greesh Sutar <girishasutar223@gmail.com>
2026-08-17 17:24:49 +0000

    Aug 17, 2026, 10:54 PM

--- utilis/mail.js
+++ utilis/mail.js
@@ -1,53 +1,6 @@
 let nodemailer = require("nodemailer");
 require("dotenv").config();
 
-<<<<<<< HEAD
-if (!process.env.RESEND_API_KEY) {
-  console.error("RESEND_API_KEY not set in environment");
-}
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-const VERIFIED_TEST_EMAIL = "girishsutar32@gmail.com";
-
-const transporter = {
-  sendMail: async ({ to, subject, text, html }) => {
-    try {
-      console.log("Sending email via Resend to:", to);
-      
-      // In testing mode, Resend only allows sending to verified email
-      const isTestMode = process.env.NODE_ENV !== "production";
-      const actualTo = isTestMode && to !== VERIFIED_TEST_EMAIL 
-        ? VERIFIED_TEST_EMAIL 
-        : to;
-      
-      if (isTestMode && to !== VERIFIED_TEST_EMAIL) {
-        console.log(`[TEST MODE] Redirecting email from ${to} to ${VERIFIED_TEST_EMAIL}`);
-      }
-      
-      const result = await resend.emails.send({
-        from: "FoodFlow <onboarding@resend.dev>",
-        to: actualTo,
-        subject,
-        text,
-        html: html || text.replace(/\n/g, "<br>")
-      });
-      console.log("Resend result:", result);
-      if (result.error) {
-        throw new Error(JSON.stringify(result.error));
-      }
-      return { messageId: result.data?.id };
-    } catch (err) {
-      console.error("Resend sendMail error:", err.message);
-      // Don't throw in test mode to allow signup to proceed
-      if (process.env.NODE_ENV !== "production") {
-        console.warn("Email failed in test mode, continuing...");
-        return { messageId: "test-mode-failed" };
-      }
-      throw err;
-    }
-  }
-};
-=======
 
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -59,6 +12,5 @@ let transporter = nodemailer.createTransport({
         pass:process.env.Email_PASS
     }
 })
->>>>>>> parent of 135bd0d (Aug 17, 2026, 4:33 PM)
 
 module.exports = transporter;
