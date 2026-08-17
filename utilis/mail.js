501bc125e9635f13fe1522b71e08a3c33f6dbcb5
Greesh Sutar <girishasutar223@gmail.com>
2026-08-17 17:26:05 +0000

    Aug 17, 2026, 10:56 PM

--- utilis/mail.js
+++ utilis/mail.js
@@ -1,6 +1,7 @@
 let nodemailer = require("nodemailer");
 require("dotenv").config();
 
+<<<<<<< HEAD
 
 let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -12,5 +13,21 @@ let transporter = nodemailer.createTransport({
         pass:process.env.Email_PASS
     }
 })
+=======
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const transporter = {
+  sendMail: async ({ to, subject, text, html }) => {
+    const result = await resend.emails.send({
+      from: "FoodFlow <onboarding@resend.dev>",
+      to,
+      subject,
+      text,
+      html: html || text.replace(/\n/g, "<br>")
+    });
+    return { messageId: result.data?.id };
+  }
+};
+>>>>>>> parent of 60237b2 (Add Resend error logging)
 
 module.exports = transporter;
